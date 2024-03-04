9504232fb310d67bf2b20913e7d42012625953ed
David Laeer <dalaeer@gmail.com>
2024-03-03 20:15:53 -0500

    updated main to select custom port

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include "defs.h"
 #include "server.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+    int port = 8080;
+    if (argc > 1) {
+        errno = 0;
+        char *endptr = nullptr;
+        port = strtol(argv[1], &endptr, 10);
+        if (errno || !endptr) {
+            std::cout << "Invalid port setting, defaulting to " << 8080 << '\n';
+            port = 8080;
+        }
+        else std::cout << "Port set to " << port << '\n';
+    }
+    else std::cout << "No port specified, defaulting to 8080\n";
     webServer server;
-    server.run(8080, LogLevel::INFO);
+    server.run(port, LogLevel::INFO);
 }
